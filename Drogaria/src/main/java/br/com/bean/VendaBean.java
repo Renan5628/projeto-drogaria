45dc771205fc67c093fe98cb97a67b9740128ee7
Desktop <renan5628@gmail.com>
2023-08-05 19:11:42 -0300

    Adição e remoção de produtos, incorporação do Finalizar Venda.

--- Drogaria/src/main/java/br/com/bean/VendaBean.java
+++ Drogaria/src/main/java/br/com/bean/VendaBean.java
@@ -1,16 +1,25 @@
 package br.com.bean;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
+import br.com.dao.ClienteDAO;
+import br.com.dao.FuncionarioDAO;
 import br.com.dao.ProdutoDAO;
+import br.com.domain.Cliente;
+import br.com.domain.Funcionario;
+import br.com.domain.ItemVenda;
 import br.com.domain.Produto;
+import br.com.domain.Venda;
 
 
 @SuppressWarnings("serial")
@@ -19,6 +28,20 @@ import br.com.domain.Produto;
 public class VendaBean implements Serializable {
 	
 	private List<Produto> produtos;
+	private List<ItemVenda> itensVenda;
+	private List<Cliente> clientes;
+	private List<Funcionario> funcionarios;
+	private Venda venda;
+	
+	
+	
+	public List<ItemVenda> getItensVenda() {
+		return itensVenda;
+	}
+	
+	public void setItensVenda(List<ItemVenda> itensVenda) {
+		this.itensVenda = itensVenda;
+	}
 	
 	
 	public List<Produto> getProdutos() {
@@ -29,14 +52,121 @@ public class VendaBean implements Serializable {
 		this.produtos = produtos;
 	}
 	
+	public Venda getVenda() {
+		return venda;
+	}
+	
+	public void setVenda(Venda venda) {
+		this.venda = venda;
+	}
+	
+	public List<Cliente> getClientes() {
+		return clientes;
+	}
+
+	public void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
+	
 	@PostConstruct
-	public void listar() {
+	public void novo() {
 		try {
+			
+			venda = new Venda();
+			venda.setPrecoTotal(new BigDecimal("00.00") );
+			
 			ProdutoDAO produtoDAO = new ProdutoDAO();
 			produtos = produtoDAO.listar("descricao");
+			
+			itensVenda = new ArrayList<>();
+			
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar carregar a tela de vendas.");
 			erro.printStackTrace();
+		}
+	}
+	
+	public void adicionar(ActionEvent event){
+		
+		Produto produto = (Produto) event.getComponent().getAttributes().get("produtoSelecionado");
+		
+		int exist = -1;
+		
+		for (int posicao = 0; posicao < itensVenda.size(); posicao++) {
+			if(itensVenda.get(posicao).getProduto().equals(produto)){
+				exist = posicao;
+			}
+		}
+		
+		if(exist < 0){
+			ItemVenda itemVenda = new ItemVenda();
+			itemVenda.setPrecoParcial(produto.getPreco());
+			itemVenda.setProduto(produto);
+			itemVenda.setQuantidade(new Short("1"));
+			
+			itensVenda.add(itemVenda);
+			
+		}else{
+			ItemVenda itemVenda = itensVenda.get(exist);
+			itemVenda.setQuantidade(new Short(itemVenda.getQuantidade() + 1 + ""));
+			itemVenda.setPrecoParcial(produto.getPreco().multiply(new BigDecimal(itemVenda.getQuantidade())));
+			
+		}
+		
+		calcular();
+		
+		
+	}
+	
+	public void remover(ActionEvent event){
+		ItemVenda itemVenda = (ItemVenda) event.getComponent().getAttributes().get("itemSelecionado");
+		
+		int exist = -1;
+		
+		for (int posicao = 0; posicao < itensVenda.size(); posicao++) {
+			if(itensVenda.get(posicao).getProduto().equals(itemVenda.getProduto())){
+				exist = posicao;
+			}
+		}
+		if(exist > -1){
+			itensVenda.remove(exist);
+		}
+		
+		calcular();
+		
+	}
+	
+	public void calcular(){
+		venda.setPrecoTotal(new BigDecimal("0.00"));
+		
+		for (int posicao = 0; posicao < itensVenda.size(); posicao++) {
+			ItemVenda itemVenda = itensVenda.get(posicao);
+			venda.setPrecoTotal(venda.getPrecoTotal().add(itemVenda.getPrecoParcial()));
+		}
+		
+	}
+	
+	public void finalizar(){
+		try {
+			FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+			funcionarios = funcionarioDAO.listarOrdenado();
+			
+			ClienteDAO clienteDAO = new ClienteDAO();
+			clientes = clienteDAO.listarOrdenado();
+			
+			
+		} catch (RuntimeException e) {
+			Messages.addGlobalError("Ocorreu um erro ao finalizar a venda.");
+			e.printStackTrace();
 		}
 	}
 	
--- Drogaria/src/test/java/br/com/util/EstadoDAOTest.java
+++ Drogaria/src/test/java/br/com/util/EstadoDAOTest.java
@@ -11,170 +11,186 @@ import br.com.dao.CidadeDAO;
 import br.com.dao.ClienteDAO;
 import br.com.dao.EstadoDAO;
 import br.com.dao.FabricanteDAO;
+import br.com.dao.FuncionarioDAO;
 import br.com.dao.PessoaDAO;
 import br.com.domain.Cidade;
 import br.com.domain.Cliente;
 import br.com.domain.Estado;
 import br.com.domain.Fabricante;
+import br.com.domain.Funcionario;
 import br.com.domain.Pessoa;
 
 public class EstadoDAOTest {
-	
+
 	@Test
 	@Ignore
-	public void salvarEstado(){
+	public void salvarEstado() {
 		Estado estado = new Estado();
-		
+
 		estado.setNome("Rio Grande do Sul");
 		estado.setSigla("RS");
-		
+
 		EstadoDAO estadoDAO = new EstadoDAO();
 		estadoDAO.salvar(estado);
-		
-		
+
 	}
-	
+
 	@Test
 	@Ignore
-	public void listarEstado(){
+	public void listarEstado() {
 		EstadoDAO estadoDAO = new EstadoDAO();
 		List<Estado> estados = estadoDAO.listar();
-		
-		for(Estado estado : estados){
-			System.out.println(estado.getNome()+" "+estado.getSigla());
+
+		for (Estado estado : estados) {
+			System.out.println(estado.getNome() + " " + estado.getSigla());
 		}
-		
+
 	}
-	
+
 	@Test
 	@Ignore
-	public void buscarEstado(){
+	public void buscarEstado() {
 		EstadoDAO estadoDAO = new EstadoDAO();
 		Estado estado = estadoDAO.buscar(3l);
-		
+
 		System.out.println(estado.getNome());
-		
-		
+
 	}
-	
+
 	@Test
 	@Ignore
-	public void excluirEstado(){
-		
+	public void excluirEstado() {
+
 		EstadoDAO estadoDAO = new EstadoDAO();
 		Estado estado = estadoDAO.buscar(3l);
-		
+
 		estadoDAO.excluir(estado);
-		
+
 		System.out.println(estado.getNome());
-		
+
 	}
-	
+
 	@Test
 	@Ignore
-	public void editarEstado(){
-		
+	public void editarEstado() {
+
 		EstadoDAO estadoDAO = new EstadoDAO();
 		Estado estado = estadoDAO.buscar(3l);
-		
+
 		estado.setNome("Minas Gerais");
 		estado.setSigla("MG");
 		estadoDAO.editar(estado);
-		
+
 		System.out.println(estado.getNome());
-		
+
 	}
-	
+
 	@Test
 	@Ignore
-	public void salvarFabricante(){
+	public void salvarFabricante() {
 		Fabricante fabricante = new Fabricante();
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
-		
+
 		fabricante.setDescricao("Aché");
-		
+
 		fabricanteDAO.salvar(fabricante);
-		
+
 	}
-	
+
 	@Test
 	@Ignore
-	public void salvarCidade() throws ParseException{
-		
+	public void salvarCidade() throws ParseException {
+
 		ClienteDAO clienteDAO = new ClienteDAO();
 		Cliente cliente = new Cliente();
 		PessoaDAO pessoaDAO = new PessoaDAO();
 		Pessoa pessoa = new Pessoa();
-		
+
 		pessoa = pessoaDAO.buscar(1l);
 		cliente.setDataDoCadastro(new SimpleDateFormat("dd/MM/yyyy").parse("29/07/2023"));
 		cliente.setLiberado(false);
 		cliente.setPessoa(pessoa);
-		
+
 		clienteDAO.merge(cliente);
-		
-		
+
 	}
 	
 	@Test
 	@Ignore
-	public void listarCidade(){
+	public void salvarFuncionario() throws ParseException{
+		
+		FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+		Funcionario funcionario = new Funcionario();
+		Pessoa pessoa = new Pessoa();
+		PessoaDAO pessoaDAO = new PessoaDAO();
+		
+		pessoa = pessoaDAO.buscar(2l);
+		funcionario.setCarteiraTrabalho("07050806");
+		funcionario.setDataAdmissao(new SimpleDateFormat("dd/MM/yyyy").parse("29/05/2023"));
+		funcionario.setPessoa(pessoa);
+		
+		funcionarioDAO.merge(funcionario);
+		
+		
+	}
+
+	@Test
+	@Ignore
+	public void listarCidade() {
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		List<Cidade> cidades = cidadeDAO.listar();
-		
+
 		System.out.println(cidades.size());
-		
+
 	}
-	
+
 	@Test
 	@Ignore
-	public void buscarCidade(){
+	public void buscarCidade() {
 		Cidade cidade = new CidadeDAO().buscar(1l);
 		System.out.println(cidade.getNome());
 	}
-	
+
 	@Test
 	@Ignore
-	public void excluirCidade(){
+	public void excluirCidade() {
 		Long codigo = 4l;
-		
+
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		Cidade cidade = new Cidade();
-		
+
 		cidade = cidadeDAO.buscar(codigo);
 		cidadeDAO.excluir(cidade);
-		
+
 	}
-	
+
 	@Test
 	@Ignore
-	public void editarCidade(){
-		
+	public void editarCidade() {
+
 		Long codigo = 2l;
-		
+
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		Cidade cidade = new Cidade();
-		
+
 		cidade = cidadeDAO.buscar(codigo);
 		cidade.setNome("Andradas");
 		cidade.setEstado(new EstadoDAO().buscar(3l));
-		
+
 		cidadeDAO.editar(cidade);
-		
+
 	}
-	
+
 	@Test
 	@Ignore
-	public void fabricanteMerge(){
+	public void fabricanteMerge() {
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		Fabricante fabricante = fabricanteDAO.buscar(1l);
-		
+
 		fabricante.setDescricao("Jhonson&Jhonson");
-		
+
 		fabricanteDAO.merge(fabricante);
-		
+
 	}
-	
-	
 
 }
